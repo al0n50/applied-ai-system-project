74475b6e1f4eed8c6c7d94ee6131e684569e051d
Richard Rodriguez <richardbr.personal@gmail.com>
2025-09-05 11:43:43 -0400

    change to copy .env.example

--- setupEnv.sh
+++ setupEnv.sh
@@ -5,16 +5,14 @@ pnpm install
 ./start-database.sh
 
 # Get POSTGRES_PASSWORD from the container and store it in a variable
-DB_PASSWORD=$(docker exec -it rentability-postgres env | grep '^POSTGRES_PASSWORD=' | cut -d '=' -f2)
+DB_PASSWORD=$(docker exec -it rentability-postgres env | grep '^POSTGRES_PASSWORD=' | cut -d '=' -f2 | tr -d '\r\n')
 
 # Print to verify
 echo "Database password: $DB_PASSWORD"
 
-# initialize the .env file to access the database
-cat <<EOF > .env
-AUTH_SECRET=""
-DATABASE_URL="postgresql://postgres:$DB_PASSWORD@localhost:5432/rentability"
-EOF
+# Copy .env.example to .env and update the database password
+cp .env.example .env
+sed -i '' "s|postgresql://postgres:password@localhost:5432/rentability|postgresql://postgres:$DB_PASSWORD@localhost:5432/rentability|" .env
 
 pnpm db:push
 
