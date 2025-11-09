d8bcefe654d0f2e0e894a36d6fd42b79f5d3b25b
Richard Rodriguez <richardbr.personal@gmail.com>
2025-11-08 20:33:17 -0500

    Updated Signin Form

---
+++ src/components/login-form.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { cn } from "~/lib/utils";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "~/components/ui/field";
+import { Input } from "~/components/ui/input";
+import { useActionState } from "react";
+import { signin } from "~/actions/auth";
+
+const initState = { message: null };
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const [formState, action] = useActionState<{ message: string | null }>(
+    // @ts-expect-error Server Action
+    signin,
+    initState,
+  );
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form action={action}>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" name="password" type="password" required />
+              </Field>
+
+              <FieldError
+                errors={
+                  formState
+                    ? [{ message: formState.message ?? undefined }]
+                    : undefined
+                }
+              />
+              <Field>
+                <Button type="submit">Login</Button>
+                <Button variant="outline" type="button" disabled>
+                  Login with Google
+                </Button>
+                <FieldDescription className="text-center">
+                  Don&apos;t have an account? <a href="#">Sign up</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
