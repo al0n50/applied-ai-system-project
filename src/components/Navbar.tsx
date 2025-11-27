cb111b0b53d30a7c53f11a707c506bcf9b2d9178
Richard Rodriguez <richardbr.personal@gmail.com>
2025-11-27 15:01:58 -0500

    Signout via avatar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,17 @@ import { Button } from "./ui/button";
 import { InputGroup, InputGroupAddon, InputGroupInput } from "./ui/input-group";
 import { Search, User } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuSeparator,
+} from "./ui/dropdown-menu";
+import { auth } from "~/server/auth";
+import DropdownSignOutItem from "~/app/DropdownSignOutItem";
 
-export default function Navbar() {
-  const user = false; // Replace with actual user authentication logic
+export default async function Navbar() {
+  const user = await auth();
   return (
     <nav className="flex gap-4 bg-white px-6 py-2 shadow-md dark:bg-neutral-950">
       <Button asChild variant="link">
@@ -29,16 +37,35 @@ export default function Navbar() {
       </InputGroup>
 
       {user ? (
-        <Avatar>
-          <AvatarImage src="" alt="User Avatar" />
-          <AvatarFallback>
-            <User />
-          </AvatarFallback>
-        </Avatar>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar>
+              <AvatarImage src={user.user?.image ?? ""} alt="User Avatar" />
+              <AvatarFallback>
+                <User />
+              </AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent sideOffset={8} align="end" className="w-44">
+            <div className="px-2 py-2 text-sm">
+              <div className="text-muted-foreground">Signed in as</div>
+              <div className="font-medium">
+                {user.user?.email ?? user.user?.name}
+              </div>
+            </div>
+            <DropdownMenuSeparator />
+            <DropdownSignOutItem />
+          </DropdownMenuContent>
+        </DropdownMenu>
       ) : (
         <>
-          <Button variant="primary">Login</Button>
-          <Button variant="secondary">Sign Up</Button>
+          <Button variant="primary" asChild>
+            <Link href="/signin">Sign In</Link>
+          </Button>
+          <Button variant="secondary" asChild>
+            <Link href="/signup">Sign Up</Link>
+          </Button>
         </>
       )}
     </nav>
